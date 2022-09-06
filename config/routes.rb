cbbe866aea58814d5744da6868df91e180ac0c9b
Chreees79 <christelle.gevaert32@gmail.com>
2022-09-06 13:21:39 +0200

    add home about contact controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/contact'
-  get 'static_pages/about'
+  get 'contact/show'
+  get 'about/show'
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
