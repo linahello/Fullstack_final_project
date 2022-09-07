c686310f1de40631a3ad246b35f485f8d830f253
Chreees79 <christelle.gevaert32@gmail.com>
2022-09-07 02:52:24 +0200

    fix some bugs in mobile first with javascript

--- app/assets/javascripts/banner.js
+++ app/assets/javascripts/banner.js
@@ -7,7 +7,7 @@ var crowd = document.getElementById('crowd');
 
 window.addEventListener('scroll', function() {
   var value = window.scrollY;
-  bg.style.top = value * 0.5 + 'px';
+  bg.style.top = value * 0.005 + 'px';
   cloud_right.style.top = value * 0.5 + 'px';
   cloud_bg.style.top = value * 0.25 + 'px';
   cloud.style.top = value * 0.15 + 'px';
