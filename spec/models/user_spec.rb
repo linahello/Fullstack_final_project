b8b4d8aa2c0b22b18c762b806cc1992123aeda3c
xko <germain.rgb@hotmail.fr>
2022-09-07 17:16:35 +0200

    spec for user (devise) created

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   before(:each) do
     @user = User.create(email: 'John@mail.com', password: 'testing')
+    @user2 = User.create(email: 'John2@mail.com', password: 'testing')
   end
 
   context 'validation' do
@@ -14,24 +15,31 @@ RSpec.describe User, type: :model do
       it 'should not be valid without email' do
         bad_user = User.create(email: 'john', password: 'testing')
         expect(bad_user).not_to be_valid
-        # test très sympa qui permet de vérifier que la fameuse formule user.errors retourne bien un hash qui contient une erreur concernant le first_name. 
         expect(bad_user.errors.include?(:email)).to eq(true)
       end
       it 'should not be valid without correct password' do
         bad_user = User.create(email: 'john@mail.com', password: 'tes')
         expect(bad_user).not_to be_valid
-        # test très sympa qui permet de vérifier que la fameuse formule user.errors retourne bien un hash qui contient une erreur concernant le first_name. 
         expect(bad_user.errors.include?(:password)).to eq(true)
       end
     end
   end
 
   context 'associations' do
-    describe 'artist_profile' do
-      it 'should has one artist_profile' do
+    describe 'user has artist profile' do
+      it 'should has one artist profile' do
         @profile_artist = ArtistProfile.create(name: 'john', description: 'je test mon code', zipcode: '31200', city: 'Toulouse', user_id: @user.id)
-        expect(@user.is_artist).to eq(true)
-        expect(@user.is_venue).to eq(false)
+        expect(@user.artist_profile.user_id).to eq(@user.id)
+        expect(@user.venue_profile).to eq(nil)
+      end
+    end
+  end
+  context 'associations' do
+    describe 'user has venue profile' do
+      it 'should has venue profile' do
+        @profile_venue = VenueProfile.create(name: 'john', description: 'je test mon code', type_of_location: 'bar', capacity: 80, address: '48 rue parla', zipcode: '31200', city: 'Toulouse', user_id: @user2.id)
+        expect(@user2.venue_profile.user_id).to eq(@user2.id)
+        expect(@user2.artist_profile).to eq(nil)
       end
     end
   end
