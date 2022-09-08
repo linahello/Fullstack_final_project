da2717cb34fd339d11ed0348dcdc4fe5e01d3ad4
GuillaumeBrthlt <bertholetguillaume@gmail.com>
2022-09-08 12:28:25 +0200

    adding back-end join table artist_genres and complete front

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :venue_profiles
   resources :artist_profiles
-  resources :artist_genres, only: %i[new create update]
+  resources :artist_genres, only: %i[new create destroy]
 
   get 'profile_selection/show'
   get 'contact/show'
