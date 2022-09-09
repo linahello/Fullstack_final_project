0a143ce830fdacb75c251df8fb838eede228528d
Chreees79 <christelle.gevaert32@gmail.com>
2022-09-09 12:31:32 +0200

    test top scroll button

--- app/assets/javascripts/topscroll.js
+++ app/assets/javascripts/topscroll.js
@@ -1,14 +1,14 @@
-var btn = $('#top_scroll');
 
-window.scroll(function() {
-  if window.scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  ('html, body').animate({scrollTop:0}, '300');
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 400) { 
+          $('#scroll').fadeIn(); 
+      } else { 
+          $('#scroll').fadeOut(); 
+      } 
+  }); 
+  $('#scroll').click(function(){ 
+      $("html").animate({ scrollTop: 0 }, 0); 
+      return false; 
+  }); 
 });
