5ecceae4064f5ff3f2bdb30244348b35f25c03d4
Chreees79 <christelle.gevaert32@gmail.com>
2022-09-12 15:49:19 +0200

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  get 'applications/new'
-  get 'applications/create'
-  get 'applications/update'
   resources :venue_profiles do
     resources :venuepicts, only: [:create]
   end
   resources :artist_profiles do
     resources :artistpicts, only: [:create]
   end
-  resources :performances
+  resources :performances do
+    resources :applications, only: %i[new create update]
+  end
   resources :artist_genres, only: %i[new create destroy]
   resources :dashboards, only: [:show]
-
+  
   get 'profile_selection/show'
   get 'contact/show'
   get 'about/show'
