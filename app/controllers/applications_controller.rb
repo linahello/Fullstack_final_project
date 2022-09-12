cb50a765270a4e474dc4b278d122a0240a28f9a4
Chreees79 <christelle.gevaert32@gmail.com>
2022-09-12 16:32:07 +0200

    work on applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationsController < ApplicationController
-  def new
-  end
+
+  def new;end
 
   def create
+    @application = Application.create(application_params)
   end
 
   def update
   end
+
+  private
+
+  def application_params
+    params.permit(:message, :status)
+  end
+
+
+
 end
