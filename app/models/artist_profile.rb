610fba02b1a0a5c167ad1b09564bc1e87478a078
LoiseauB <bapt.loiseau@gmail.com>
2022-09-12 17:49:42 +0200

    add validate to active storage

--- app/models/artist_profile.rb
+++ app/models/artist_profile.rb
@@ -9,4 +9,31 @@ class ArtistProfile < ApplicationRecord
   has_many :applications
   has_many :performances, through: :applicaitons
   has_one_attached :artistpict
+  validate :artistpict_format
+
+  def resize_image
+    resized_image = MiniMagick::Image.read(artistpict.download)
+    resized_image = resized_image.resize '500x500'
+    v_filename = artistpict.filename
+    v_content_type = artistpict.content_type
+    artistpict.purge
+    artistpict.attach(io: File.open(resized_image.path), filename: v_filename, content_type: v_content_type)
+  end
+
+  private
+
+  def artistpict_format
+    return unless artistpict.attached?
+    if artistpict.blob.content_type.start_with? 'image/'
+      if artistpict.blob.byte_size > 1.megabytes
+        errors.add(:artistpict, 'size needs to be less than 1MB')
+        artistpict.purge
+      else
+        resize_image
+      end
+    else
+      artistpict.purge
+      errors.add(:artistpict, 'needs to be an image')
+    end
+  end
 end
