43609df54e160f53dd871d7dafd944582a697810
LoiseauB <bapt.loiseau@gmail.com>
2022-09-12 10:32:18 +0200

    add active storage & update artist and venue model

--- app/models/artist_profile.rb
+++ app/models/artist_profile.rb
@@ -6,4 +6,5 @@ class ArtistProfile < ApplicationRecord
   validates :zipcode, presence: true, format: { with: /\A(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}\z/, message: 'Code postal invalide' }
   has_many :artist_genres, dependent: :destroy
   has_many :genres, through: :artist_genres
+  has_one_attached :artistpict
 end
