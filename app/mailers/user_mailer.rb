5c8256a0e34d80ae4aaaf65de3fcf9296e8e9426
Chreees79 <christelle.gevaert32@gmail.com>
2022-09-13 11:46:28 +0200

    work in progess for venue's email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,13 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
 
+  def application_send 
+    @venue = 
+  end
 end
+  def admin_email(order)
+    @users = User.where(admin: true)
+    @order = order
+    emails = @users.collect(&:email).join(",")
+    mail(to: emails, subject: 'Une commande a été passée sur votre site')
+  end
