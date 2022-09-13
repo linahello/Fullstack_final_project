8e7c6137036d883acd17b03913e75def8d731a2b
Chreees79 <christelle.gevaert32@gmail.com>
2022-09-13 14:01:37 +0200

    add method and view for venue's email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,14 +5,7 @@ class UserMailer < ApplicationMailer
     @url = 'https://place-to-play.herokuapp.com/'
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
-
-  def application_send 
-    @venue = 
+  def application_send(user)
+    mail(to: user.email, subject: 'Nouvelle candidature sur un de vos évènements') 
   end
 end
-  def admin_email(order)
-    @users = User.where(admin: true)
-    @order = order
-    emails = @users.collect(&:email).join(",")
-    mail(to: emails, subject: 'Une commande a été passée sur votre site')
-  end
--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,10 @@ class Application < ApplicationRecord
   enum status: %i[pending accepted declined]
   belongs_to :artist_profile
   belongs_to :performance
+  after_save :email_venue_send
+
+  def email_venue_send
+  user = self.performance.venue_profile.user
+  UserMailer.application_send(user).deliver_now
+  end
 end
