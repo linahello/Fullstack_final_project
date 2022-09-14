a0aff97f13bc29bfdd80ad728f8520761b47a65e
Chreees79 <christelle.gevaert32@gmail.com>
2022-09-14 18:22:43 +0200

    job well done

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,22 +1,30 @@
 class ApplicationsController < ApplicationController
 
-  def new; end
+  def new
+  end
 
   def create
    @application = Application.new(application_params)
   @application.update(artist_profile_id: current_user.artist_profile.id)
   @application.save!
-  redirect_to performance_application_path(id: @application.id)
+  redirect_to dashboard_path(current_user.id)
+
+end
+
+  def update
+  @application = Application.find(params[:id])
+  @application.update(application_params)
+  redirect_to dashboard_path(current_user.id)
   end
 
-  def update; end
-
-  def show; end
+  def show 
+  @application = Application.find(params[:id])
+  end
 
   private 
 
   def application_params
-    params.permit(:performance_id, :message)
+    params.permit(:performance_id, :message, :status)
   end
 
 
